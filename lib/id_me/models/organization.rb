ef31c6c7e8126cd860ebcf9841767d7a6b0af14b
Pat Newell <pnewell+git@renttherunway.com>
2014-03-18 08:24:12 -0400

    wip: user/profile/databit models

--- lib/id_me/models/organization.rb
+++ lib/id_me/models/organization.rb
@@ -3,16 +3,23 @@ require_relative 'sport'
 
 module Sprtid
 	module Models
-		class Sport < Base
-			field :name, :type => String
-
-			belongs_to :organization, :class_name => 'Sprtid::Models::Organization', :inverse_of => :sports
-		end
 		class Organization < Base
 			field :name, :type => String
 
 			embeds_many :sports, :class_name => 'Sprtid::Models::Sport', :inverse_of => :organization
 			has_many :events, :class_name => 'Sprtid::Models::Event', :inverse_of => :organization
+		end
+
+		class Team < Base
+			field :name, :type => String
+
+			embeds_many :players, :class_name => 'Sprtid::Models::Identity', :inverse_of => :sport
+			belongs_to :organization, :class_name => 'Sprtid::Models::Organization', :inverse_of => :teams
+
+			# which is it?
+			# has_one :sport, :class_name => 'Sprtid::Models::Sport', :inverse_of => :teams
+			# belongs_to :sport, :class_name => 'Sprtid::Models::Sport', :inverse_of => :teams
+			embeds_one :sport, :class_name => 'Sprtid::Models::Sport', :inverse_of => :teams
 		end
 	end
 end
